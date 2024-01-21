5f2f436e7b36267ad45558637ef30f150f932928
wwnOOv <145524959+wwnoov@users.noreply.github.com>
2024-01-21 11:41:25 +0900

    Springboot basic setting

---
+++ btest00/src/main/java/com/study/springboot/Btest00Application.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Btest00Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Btest00Application.class, args);
+    }
+
+}
