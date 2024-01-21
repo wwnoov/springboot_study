c23afa389e1c6ebf6b4ef36bd3d6c8c1eb1392fd
wwnOOv <145524959+wwnoov@users.noreply.github.com>
2024-01-21 11:39:50 +0900

    Springboot basic setting

---
+++ sbt00/src/main/java/kr/co/chunjae/controller/Sbt00Application.java
@@ -0,0 +1,13 @@
+package kr.co.chunjae.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbt00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbt00Application.class, args);
+	}
+
+}
---
+++ sbt00/src/main/java/kr/co/chunjae/controller/TestController.java
@@ -0,0 +1,14 @@
+package kr.co.chunjae.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping(value="/")
+	public String home() {
+		System.out.println("TestController home");
+		return "index";
+	}
+}
