6785a9cc2060ac3a5e903f39a961ce8c10e0eab7
wwnOOv <145524959+wwnoov@users.noreply.github.com>
2024-01-21 11:40:36 +0900

    Springboot basic setting

---
+++ sbt01/src/main/java/kr/co/chunjae/controller/Sbt01Application.java
@@ -0,0 +1,13 @@
+package kr.co.chunjae.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbt01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbt01Application.class, args);
+	}
+
+}
