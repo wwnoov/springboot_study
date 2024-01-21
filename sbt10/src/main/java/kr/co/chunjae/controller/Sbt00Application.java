661e68b5ef2d6ceb08446faea33f68fe7e56a016
wwnOOv <145524959+wwnoov@users.noreply.github.com>
2024-01-22 07:22:23 +0900

    SpringBoot basic setting

---
+++ sbt10/src/main/java/kr/co/chunjae/controller/Sbt00Application.java
@@ -0,0 +1,15 @@
+package kr.co.chunjae.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Sbt00Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbt00Application.class, args);
+    }
+
+}
